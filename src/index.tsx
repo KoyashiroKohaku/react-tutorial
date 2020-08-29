a159d497c92ab101d94fd5c05a3caf00a34c79e3
Koyashiro Kohaku <develop@koyashiro.net>
2020-08-29 19:04:48 +0900

    feat: tic-tac-toe game

--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+interface Props {
+  value: string,
+  onClick(): void,
 }
 
-class Board extends React.Component {
+interface State {
+  squares: Array<string>,
+  xIsNext: boolean,
+}
+
+function Square(props: Props) {
+  return (
+    <button
+      className="square"
+      onClick={() => props.onClick()}>
+      {props.value}
+    </button >
+  );
+}
+
+class Board extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      squares: Array<string>(9).fill(''),
+      xIsNext: true,
+    }
+  }
+
+  static calculateWinner(squares: Array<string>): string | null {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of lines) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  handleClick(i: number) {
+    const squares = this.state.squares.slice();
+    if (Board.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+  }
+
   renderSquare(i: number) {
-    return <Square />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = Board.calculateWinner(this.state.squares);
+    const status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -48,7 +100,7 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board prop={{}} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
